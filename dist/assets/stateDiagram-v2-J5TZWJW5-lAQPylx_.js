4166f615af53c23c2d5d127b839b4b10ab8ad093
AppElent <ericjansen@live.nl>
2024-10-11 06:43:02 +0000

    Remove unnecessary files and code

--- dist/assets/stateDiagram-v2-J5TZWJW5-lAQPylx_.js
+++
@@ -1,1 +0,0 @@
-import{s as t,a as e,c as r,b as s}from"./chunk-2JBRQKJ5-flAgUNKw.js";import{_ as i}from"./index-Bh7s61El.js";import"./chunk-YWFND7JV-HR3XuPIV.js";import"./chunk-DUMQOTYW-BvskSbof.js";var _={parser:t,db:e,renderer:r,styles:s,init:i(a=>{a.state||(a.state={}),a.state.arrowMarkerAbsolute=a.arrowMarkerAbsolute,e.clear()},"init")};export{_ as diagram};
